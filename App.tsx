5a73d2c09b0e34861f3cfebdbb1542df297bc00b
Moises <moises@imaxis.com.br>
2024-09-09 09:19:08 -0300

    [Feat] loading home after building font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import Home from "@screens/Home";
 
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from '@theme/index';
+
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
 
 export default function App() {
+  const [fontsLoaded] =  useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <Home>
-
-    </Home>
+    <ThemeProvider theme={theme}>
+       { fontsLoaded ? <Home /> : <Loading /> }
+    </ThemeProvider>
   );
 }
 
