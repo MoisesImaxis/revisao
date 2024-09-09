0da10baff674d461d63401b49d4e3593d01e5944
Moises <moises@imaxis.com.br>
2024-09-09 09:05:42 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { Container } from "./style";
+
+export default function Home(){
+    return(
+        <Container>
+            <Text>
+                Teste
+            </Text>
+        </Container>
+    )
+}
